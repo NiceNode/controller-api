8003ad797b8b11b9cd4db4040bf82bae8a84a9a7
jgresham <johnsgresham@gmail.com>
2024-08-04 09:02:51 -0700

    feat: op-reth for optimism and base

--- src/app/api/controller/librarySpecs.ts
+++ src/app/api/controller/librarySpecs.ts
@@ -26,6 +26,7 @@ import hildrv1 from './NodeSpecs/hildr/hildr-v1.0.0.json';
 import magiv1 from './NodeSpecs/magi/magi-v1.0.0.json';
 import opGethv1 from './NodeSpecs/op-geth/op-geth-v1.0.0.json';
 import opNodev1 from './NodeSpecs/op-node/op-node-v1.0.0.json';
+import opRethv1 from './NodeSpecs/op-reth/op-reth-v1.0.0.json';
 
 import hubblev1 from './NodeSpecs/hubble/hubble-v1.0.0.json';
 
@@ -35,34 +36,35 @@ import homeAssistantServicev1 from './NodeSpecs/home-assistant-service/home-assi
 import itzgMinecraftv1 from './NodeSpecs/itzg-minecraft/itzg-minecraft-v1.0.0.json';
 
 export const specs = [
-    besuv1,
-    nethermindv1,
-    erigonv1,
-    gethv1,
-    rethv1,
-    lodestarv1,
-    nimbusv1,
-    tekuv1,
-    lighthousev1,
-    prysmv1,
-    arbitrumv1,
-    nitrov1,
-    pathfinderv1,
-    opGethv1,
-    opNodev1,
-    hildrv1,
-    magiv1,
-    hubblev1,
-    itzgMinecraftv1,
-    homeAssistantServicev1,
-  ];
+  besuv1,
+  nethermindv1,
+  erigonv1,
+  gethv1,
+  rethv1,
+  lodestarv1,
+  nimbusv1,
+  tekuv1,
+  lighthousev1,
+  prysmv1,
+  arbitrumv1,
+  nitrov1,
+  pathfinderv1,
+  opGethv1,
+  opNodev1,
+  opRethv1,
+  hildrv1,
+  magiv1,
+  hubblev1,
+  itzgMinecraftv1,
+  homeAssistantServicev1,
+];
 
-  export const packageSpecs = [
-    ethereumv1,
-    farcasterv1,
-    arbitrumv1,
-    optimismv1,
-    basev1,
-    minecraftv1,
-    homeAssistantv1,
-  ];
+export const packageSpecs = [
+  ethereumv1,
+  farcasterv1,
+  arbitrumv1,
+  optimismv1,
+  basev1,
+  minecraftv1,
+  homeAssistantv1,
+];
